18675f0e984a2fad62c29cfb0ddea35ad7effbfc
Mikolaj Ostrowski <mikolaj.p.ostrowski@gmail.com>
2025-03-05 19:33:19 +0100

    Initial Commit

---
+++ djiRouteLoader/djiRouteLoaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  djiRouteLoaderApp.swift
+//  djiRouteLoader
+//
+//  Created by Mikolaj Ostrowski on 5/3/25.
+//
+
+import SwiftUI
+
+@main
+struct djiRouteLoaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
