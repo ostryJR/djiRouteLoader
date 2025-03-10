7952c32e4be9174c729c7ee39c11574ccc6a6c67
Mikolaj Ostrowski <mikolaj.p.ostrowski@gmail.com>
2025-03-10 13:22:17 +0100

    conecting to sqlite file finished main page and info page finnished

--- djiRouteLoader/MainView.swift
+++ djiRouteLoader/MainView.swift
@@ -8,14 +8,92 @@
 import SwiftUI
 
 struct MainView: View {
+    @State public var dbData:[Map] = []
     var body: some View {
         NavigationStack {
             VStack{
-                Text("Home")
-                Button("connect"){connectToDatabase()}
+                if(dbData.isEmpty){
+                    Text("Click the Connect (\(Image(systemName: "app.connected.to.app.below.fill")))")
+                    Text("button to load data from database")
+                    .background(Image(systemName: "arrow.up.backward.square").font(.system(size: 250)).opacity(0.05))
+                }else{
+                    List{
+                        ForEach(dbData, id: \.missionId){ map in
+                            NavigationLink(destination: {
+                                //Info of specific map
+                                
+                                List{
+                                    Image(systemName: "photo.on.rectangle.fill")
+                                        .padding(10)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        //.frame(width: 30, height: 30)
+                                    HStack{
+                                        Text("Mission Id:").padding(10);Spacer();
+                                        Text("\(map.missionId)").padding(10)}
+                                    HStack{
+                                        Text("Mission Name:").padding(10);Spacer();
+                                        Text("\(map.name)").padding(10)}
+                                    HStack{
+                                        Text("Author:").padding(10);Spacer();
+                                        Text("\(map.author)").padding(10)}
+                                    HStack{
+                                        Text("Created @:").padding(10);Spacer();
+                                        Text("\(NSDate(timeIntervalSince1970:map.createdTime))").padding(10)}
+                                    HStack{
+                                        Text("Modified @:").padding(10);Spacer();
+                                        Text("\(NSDate(timeIntervalSince1970:map.modifiedTime))").padding(10)}
+                                    HStack{
+                                        Text("Waypoint #:").padding(10);Spacer();
+                                        Text("\(map.waypointCount)").padding(10)}
+                                    HStack{
+                                        Text("Mileage:").padding(10);Spacer();
+                                        Text("\(Int(map.mileage)) m").padding(10)}
+                                    HStack{
+                                        Text("Wayline Latitude:").padding(10);Spacer();
+                                        Text("\(map.waylineLatitude)").padding(10)}
+                                    HStack{
+                                        Text("Wayline Longitude:").padding(10);Spacer();
+                                        Text("\(map.waylineLongitude)").padding(10)}
+                                    HStack{
+                                        Text("Location Desc:").padding(10);Spacer();
+                                        Text("\(map.locationDes)").padding(10)}
+                                
+                            }.navigationTitle("Info")
+                            .navigationBarTitleDisplayMode(.inline)
+                            }){
+                                HStack{
+//                                    if(map.coverImagePath == ""){
+                                        Image(systemName: "map")
+                                            .resizable()
+                                            .frame(width: 30, height: 30)
+//                                    }else{
+//                                        var path = map.missionId+"/mission_cover/"+map.coverImagePath
+//                                        Image(uiImage: UIImage(path: path))
+//                                            .resizable()
+//                                            .frame(width: 30, height: 30)
+//                                    }
+                                    VStack{
+                                        Text("\(map.name)").lineLimit(1).frame(maxWidth: .infinity, alignment: .leading)
+                                        Text("\(map.author)")/*.monospaced()*/.frame(maxWidth: .infinity, alignment: .leading).foregroundStyle(.secondary)
+                                    }
+                                }
+                                
+                            }.navigationTitle("Main")
+                            .navigationBarTitleDisplayMode(.inline)
+                        }
+                    }.cornerRadius(20)
+                }
             }.padding()
-                    
             .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        dbData = connectToDatabase()
+                    } label: {
+                        Image(systemName: "app.connected.to.app.below.fill")
+                    }
+                    //Image(systemName: "app.connected.to.app.below.fill")
+                }
                 ToolbarItem(placement: .navigationBarTrailing) {
                     NavigationLink {
                         AddNewView()
@@ -25,8 +103,17 @@ struct MainView: View {
 
                 }
             }
+//            .frame(maxWidth: .infinity, maxHeight: .infinity)
+//            .background {
+//                Color.gray.opacity(0.1)
+//                    .ignoresSafeArea()
+//            }
+        }.task{
+            do{
+                dbData = await connectToDatabase()
+                print(dbData[0].filePaths)
+            }}
         }
-    }
 }
 
 struct AddNewView: View {
--- djiRouteLoader/functions.swift
+++ djiRouteLoader/functions.swift
@@ -8,6 +8,76 @@
 import Foundation
 import SQLite
 
+class Map{
+    var missionId: String
+    var filePaths: String
+    var name: String
+    var author: String
+    var createdTime: Double
+    var modifiedTime: Double
+    var coverImagePath: String
+    var waypointImageNames: String
+    var poiImageNames: String
+    var waypointCount: Int
+    var mileage: Double
+    var waylineLatitude: Double
+    var waylineLongitude: Double
+    var locationDes: String
+    
+    init(){
+        self.missionId = UUID().uuidString
+        self.filePaths = "/var/mobile/Containers/Data/Application/"+missionId+"/Documents/wayline_mission/"+missionId+"/"+missionId+".kmz"
+        self.name = "-"
+        self.author = "-"
+        self.createdTime = 0
+        self.modifiedTime = 0
+        self.coverImagePath = ""
+        self.waypointImageNames = ""
+        self.poiImageNames = ""
+        self.waypointCount = 0
+        self.mileage = 0
+        self.waylineLatitude = 0
+        self.waylineLongitude = 0
+        self.locationDes = ""
+    }
+    init(missionId: String, filePaths: String, name: String, author: String, createdTime: Double, modifiedTime: Double, coverImagePath: String, waypointImageNames: String, poiImageNames: String, waypointCount: Int, mileage: Double, waylineLatitude: Double, waylineLongitude: Double, locationDes: String){
+        self.missionId = missionId
+        self.filePaths = filePaths
+        self.name = name
+        self.author = author
+        self.createdTime = createdTime
+        self.modifiedTime = modifiedTime
+        self.coverImagePath = coverImagePath
+        self.waypointImageNames = waypointImageNames
+        self.poiImageNames = poiImageNames
+        self.waypointCount = waypointCount
+        self.mileage = mileage
+        self.waylineLatitude = waylineLatitude
+        self.waylineLongitude = waylineLongitude
+        self.locationDes = locationDes
+    }
+    //init just for testing with changed path
+    init(name: String, author: String){
+        self.missionId = UUID().uuidString
+        self.filePaths = "/Users/miki/Desktop/Development/djiWaypointRouteLoader/djiRouteLoader/djiRouteLoader/wayline_mission/"+missionId+"/"+missionId+".kmz"
+        self.name = name
+        self.author = author
+        self.createdTime = 0
+        self.modifiedTime = 0
+        self.coverImagePath = ""
+        self.waypointImageNames = ""
+        self.poiImageNames = ""
+        self.waypointCount = 0
+        self.mileage = 0
+        self.waylineLatitude = 0
+        self.waylineLongitude = 0
+        self.locationDes = ""
+    }
+    func genSQL() -> String{
+        return "INSERT INTO 'kmzTable' VALUES ('\(missionId)', '\(filePaths)', '\(name)', '\(author)', '\(createdTime)', '\(modifiedTime)', '\(coverImagePath)', '\(waypointImageNames)', '\(poiImageNames)', '\(waypointCount)', '\(mileage)', '\(waylineLatitude)', '\(waylineLongitude)', '\(locationDes)');"
+    }
+}
+
 func printPath(){
     
     let fileManager = FileManager.default
@@ -27,49 +97,29 @@ func printPath(){
     }
 }
 
-func connectToDatabase(){
+func connectToDatabase() -> [Map]{
     //printPath()
     do {
         let db = try Connection("/Users/miki/Desktop/Development/djiWaypointRouteLoader/djiRouteLoader/djiRouteLoader/wayline_mission/mission_db/wpmz.sqlite3")
-
-//        let users = Table("users")
-//        let id = Expression<Int64>("id")
-//        let name = Expression<String?>("name")
-//        let email = Expression<String>("email")
-//
-//        try db.run(users.create { t in
-//            t.column(id, primaryKey: true)
-//            t.column(name)
-//            t.column(email, unique: true)
-//        })
-//        // CREATE TABLE "users" (
-//        //     "id" INTEGER PRIMARY KEY NOT NULL,
-//        //     "name" TEXT,
-//        //     "email" TEXT NOT NULL UNIQUE
-//        // )
-//
-//        let insert = users.insert(name <- "Alice", email <- "alice@mac.com")
-//        let rowid = try db.run(insert)
-//        // INSERT INTO "users" ("name", "email") VALUES ('Alice', 'alice@mac.com')
-//
-//        for user in try db.prepare(users) {
-//            print("id: \(user[id]), name: \(user[name]), email: \(user[email])")
-//            // id: 1, name: Optional("Alice"), email: alice@mac.com
-//        }
-//        // SELECT * FROM "users"
-//
-//        let alice = users.filter(id == rowid)
-//
-//        try db.run(alice.update(email <- email.replace("mac.com", with: "me.com")))
-//        // UPDATE "users" SET "email" = replace("email", 'mac.com', 'me.com')
-//        // WHERE ("id" = 1)
-//
-//        try db.run(alice.delete())
-//        // DELETE FROM "users" WHERE ("id" = 1)
-//
-//        try db.scalar(users.count) // 0
-//        // SELECT count(*) FROM "users"
+// SQL command:
+//        INSERT INTO 'kmzTable' VALUES ('A4F24663-7677-458A-BA6C-0992DAC0367C', '/var/mobile/Containers/Data/Application/277FDE7D-6245-49DE-B9DE-DEC9E1BA6C94/Documents/wayline_mission/A4F24663-7677-458A-BA6C-0992DAC0367C/A4F24663-7677-458A-BA6C-0992DAC0367C.kmz', 'artificially added', 'mikpos', '17411978.26098', '17411978.26180', 'cover2.jpg', '0,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,', '', '37', '4498.531086534701', '50.277011544017', '18.6436508510853', '44-122');
+        
+        var dbRecords:[Map] = []
+        let newRecord = Map(name: "mapTest001", author: "miki")
+        
+        
+        //let kmzTable = Table("kmzTable")
+        
+        let stmt = try db.prepare("SELECT * FROM 'kmzTable';")
+        for rowDB in stmt {
+            var rowMap:Map
+            rowMap = Map(missionId: rowDB[0] as! String, filePaths: rowDB[1] as! String, name: rowDB[2] as! String, author: rowDB[3] as! String, createdTime: rowDB[4] as! Double, modifiedTime: rowDB[5] as! Double, coverImagePath: rowDB[6] as! String, waypointImageNames: rowDB[7] as! String, poiImageNames: rowDB[8] as! String, waypointCount: Int(truncatingIfNeeded: rowDB[9] as! Int64) , mileage: rowDB[10] as! Double, waylineLatitude: rowDB[11] as! Double, waylineLongitude: rowDB[12] as! Double, locationDes: rowDB[13] as! String)
+            dbRecords.append(rowMap)
+        }
+        return dbRecords
+        
     } catch {
         print (error)
     }
+    return []
 }
